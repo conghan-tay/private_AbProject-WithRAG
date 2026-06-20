573bdb200fda60de33428d2e1d982c306a60416f
conghan tay <conghantay@conghans-MacBook-Air.local>
2026-06-20 11:22:16 +0800

    use contants

---
+++ backend/files/rag_protocol.py
@@ -0,0 +1,36 @@
+ACTION_ASK = "ask"
+ACTION_SELECT = "select"
+
+FIELD_ACTION = "action"
+FIELD_CODE = "code"
+FIELD_DATA = "data"
+FIELD_FILE_IDS = "file_ids"
+FIELD_INDEXED_FILES = "indexed_files"
+FIELD_QUESTION = "question"
+FIELD_REASON = "reason"
+FIELD_SKIPPED_FILES = "skipped_files"
+FIELD_SOURCES = "sources"
+FIELD_STATE = "state"
+FIELD_TYPE = "type"
+
+MESSAGE_TYPE_DONE = "done"
+MESSAGE_TYPE_ERROR = "error"
+MESSAGE_TYPE_NO_ANSWER = "no_answer"
+MESSAGE_TYPE_READY = "ready"
+MESSAGE_TYPE_STATUS = "status"
+MESSAGE_TYPE_TOKEN = "token"
+
+STATE_ANSWERING = "answering"
+STATE_CONNECTED_NO_DOCUMENTS = "connected_no_documents"
+STATE_DISCONNECTED = "disconnected"
+STATE_INGESTING = "ingesting"
+STATE_READY = "ready"
+
+ERROR_ALREADY_SELECTED = "already_selected"
+ERROR_BAD_REQUEST = "bad_request"
+ERROR_BUSY = "busy"
+ERROR_NO_DOCUMENTS = "no_documents"
+ERROR_NOT_READY = "not_ready"
+
+CLOSE_CODE_MISSING_USER_ID = 4401
+CLOSE_CODE_BLANK_USER_ID = 4400
